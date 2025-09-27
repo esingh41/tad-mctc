5d17468b91e90575d797bcc9e496676c14edf2d9
Elizabeth Singh <esingh41@gatech.edu>
2025-09-27 13:41:49 -0400

    intermolecular approach batching works

--- src/tad_mctc/batch/mask/pairs.py
+++ src/tad_mctc/batch/mask/pairs.py
@@ -23,7 +23,7 @@ Functions for creating masks that discern between padding and actual values.
 from __future__ import annotations
 
 import torch
-
+import re
 from ...typing import Tensor
 from .atoms import real_atoms
 
@@ -31,7 +31,7 @@ __all__ = ["real_pairs"]
 
 
 # scripting or tracing does not improve performance
-def real_pairs(numbers: Tensor, mask_diagonal: bool = True) -> Tensor:
+def real_pairs(numbers: Tensor, mask_diagonal: bool = True, mon_A_indices=None, mon_B_indices=None) -> Tensor:
     """
     Create a mask for pairs of atoms from atomic numbers, discerning padding
     and actual atoms. Padding value is zero.
@@ -50,11 +50,11 @@ def real_pairs(numbers: Tensor, mask_diagonal: bool = True) -> Tensor:
         Mask for atom pairs that discerns padding and real atoms.
     """
     if mask_diagonal is True:
-        return real_pairs_maskdiag(numbers)
+        return real_pairs_maskdiag(numbers, mon_A_indices, mon_B_indices)
     return real_pairs_no_maskdiag(numbers)
 
 
-def real_pairs_maskdiag(numbers: Tensor) -> Tensor:
+def real_pairs_maskdiag(numbers: Tensor, mon_A_indices=None, mon_B_indices=None) -> Tensor:
     """
     Create a mask for pairs of atoms from atomic numbers, discerning padding
     and actual atoms. Padding value is zero.
@@ -70,10 +70,36 @@ def real_pairs_maskdiag(numbers: Tensor) -> Tensor:
         Mask for atom pairs that discerns padding and real atoms.
     """
     real = real_atoms(numbers)
-    mask = real.unsqueeze(-2) * real.unsqueeze(-1)
-    mask *= ~torch.diag_embed(torch.ones_like(real))
-    return mask
 
+    if mon_A_indices is not None and mon_B_indices is not None:
+        print(f"{numbers= }")
+        maskA = torch.zeros_like(real)
+        maskB = torch.zeros_like(real)
+
+        batch_size = maskA.shape[0]
+        rows = torch.arange(batch_size).unsqueeze(1)
+
+        maskA[rows, mon_A_indices] = True
+        print(f"{maskA= }")
+        maskB[rows, mon_B_indices] = True
+        #I am setting maskB zero index to False, because molecule B will never have atom 0 
+        #if I use the qcelemental, and then they pad with zeros, so the code is accidentally assigning 
+        #atom 0 to A and B
+        maskB[rows, 0] = False
+        print(f"{maskB= }")
+
+
+        #Get atom pairs between mon A and mon B, double count on purpose cuz they * by 0.5
+        AB = maskA.unsqueeze(-2) * maskB.unsqueeze(-1)
+        BA = maskB.unsqueeze(-2) * maskA.unsqueeze(-1)
+        mask = AB | BA
+        print("Mask sum per batch:", mask.sum(dim=(-1,-2)))
+
+
+    else:
+        mask = real.unsqueeze(-2) * real.unsqueeze(-1)
+        mask *= ~torch.diag_embed(torch.ones_like(real))
+    return mask
 
 def real_pairs_no_maskdiag(numbers: Tensor) -> Tensor:
     """
